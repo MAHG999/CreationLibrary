ed09231557f88641f641ca2ac0a1fad14bd06cd2
Marco <antnyhern@gmail.com>
2021-06-13 22:56:32 -0500

    Add all project

---
+++ Aritmetica/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aritmetica
+{
+    public class MisMates
+    {
+        private double a;
+        private double b;
+        private double r;
+        /// <summary>
+        /// MisMates recives two values of type double
+        /// </summary>
+        /// <param name="pA"></param>
+        /// <param name="pB"></param>
+        public MisMates(double pA, double pB)
+        {
+            a = pA;
+            b = pB;
+            r = 0;
+        }
+
+        public double R { get { return r; } }
+        
+        public double suma()
+        {
+            r = a + b;
+            return r;
+        }
+
+        public double resta()
+        {
+            r = a - b;
+            return r;
+        }
+
+        public double multi()
+        {
+            r = a * b;
+            return r;
+        }
+
+        public double div()
+        {
+            r = a / b;
+            return r;
+        }
+    }
+}
---
+++ ComsumeLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Aritmetica;
+
+namespace ComsumeLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarates of variable with values from the Library
+            MisMates operation = new MisMates(5,10.5);
+            //Optains the value of the operation
+            double add = operation.suma();
+            //Print the value of the answer
+            Console.WriteLine("The result of the add is {0}", add);
+            Console.ReadKey();
+        }
+    }
+}
